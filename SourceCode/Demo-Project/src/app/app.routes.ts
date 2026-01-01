9cb26573015a07b881182fe96bf4f77e0706115c
DESKTOP-3KKODGM\dwara <dwarakesh@snovasys.com>
2026-01-01 16:52:46 +0530

    New-Project_Deployed

---
+++ SourceCode/Demo-Project/src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { DemoMainComponent } from './components/demo-main/demo-main.component';
+export const routes: Routes = [
+  {
+    path: '',
+    component: DemoMainComponent,
+  }
+];
---
+++ SourceCode/Demo-Project/src/app/components/demo-main/demo-main.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demo-main',
+  standalone: true,
+  imports: [],
+  templateUrl: './demo-main.component.html',
+  styleUrl: './demo-main.component.scss'
+})
+export class DemoMainComponent {
+
+}
