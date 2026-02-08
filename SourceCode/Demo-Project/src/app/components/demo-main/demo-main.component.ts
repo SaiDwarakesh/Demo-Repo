5225bd0d8d66341b90006483da88f869dd7316dd
Brahmendra Vadarevu <vadarevubrahendra123@gmail.com>
2026-02-08 13:51:21 +0530

    footer cmponent added

--- SourceCode/Demo-Project/src/app/components/demo-main/demo-main.component.ts
+++ SourceCode/Demo-Project/src/app/components/demo-main/demo-main.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-demo-main',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent],
   templateUrl: './demo-main.component.html',
   styleUrl: './demo-main.component.scss'
 })
